a0efe515f5f3e77a5c2ea00dd2727ca5b59750f9
Esteban Ibañez <e.ibannez.p@gmail.com>
2020-04-14 20:16:39 -0400

    estructura web v1.0

--- process.php
+++ process.php
@@ -1,17 +1,16 @@
 <?php
-/*
-	Creado por DaríoBF - www.dariobf.com
-	Script que gestiona el envío de un formulario por correo electrónico a la cuenta indicada.
-*/
 
 //Correo de destino; donde se enviará el correo.
-$correoDestino = "e.ibannez.p@gmail.com";
+$correoDestino = "ventas@dankconsultores.cl";
 
 //Texto emisor; sólo lo leerá quien reciba el contenido.
 $textoEmisor = "MIME-VERSION: 1.0\r\n";
-$textoEmisor .= "Content-type: text/html; charset=UTF-8\r\n";
-$textoEmisor .= "From: Formulario creado por DarioBF - www.dariobf.com";
 
+$textoEmisor = "MIME-Version: 1.0" . "\r\n";
+$textoEmisor .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+// $textoEmisor .= "From: Formulario creado por dankconsultores web";
+
+// $textoEmisor = "From: e.ibannez.p@gmail.com" . "\r\n" . "CC: pa.ordenes.va@gmail.com";
 /*
 	Recopilo los datos vía POST
 	Con strip_tags suprimo etiquetas HTML y php para evitar una posible inyección.
@@ -23,17 +22,17 @@ $empresa = strip_tags($_POST['empresa']);
 // $departamento = strip_tags($_POST['department']);
 $telefono = strip_tags($_POST['telefono']);
 $correo = strip_tags($_POST['mail']);
-$comentario = strip_tags($_POST['comment']);
+$comentario = strip_tags($_POST['message']);
 $fecha = time();
 $fechaFormateada = date("j/n/Y", $fecha);
 
 //Formateo el asunto del correo
-$asunto = "Contacto WEB_$nombre; de la $empresa";
+$asunto = "Contacto vía WEB Dank Consultores";
 
 //Formateo el cuerpo del correo
 
-$cuerpo = "<b>Enviado por:</b> " . $nombre . ",  a las " . $fechaFormateada . "<br />";
-$cuerpo .= "<b>Empresa:</b> " . $empresa . ",<br />";
+$cuerpo = "<b>Solicitud de:</b> " . $nombre . ",  a las " . $fechaFormateada . "<br />";
+$cuerpo .= "<b>Empresa:</b> " . $empresa . "<br />";
 $cuerpo .= "<b>Teléfono de contacto: </b>" . $telefono . "<br />";
 $cuerpo .= "<b>E-mail:</b> " . $correo . "<br />";
 $cuerpo .= "<b>Comentario:</b> " . $comentario;
